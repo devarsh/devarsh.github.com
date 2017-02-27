fa1a1eb9eccc6276dbd67398082a862074294881
devarsh shah <devarshmshah@gmail.com>
2017-02-28 02:51:37 +0530

    refactoring code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,8 @@ import css from './Layout.css'
 import {Header} from './Header.js'
 import {Content} from './Content.js'
 
-
-
 export class Layout extends Component {
-
   render() {
-    console.log(css)
     return (
       <div className={css["layout"]}>
         <div className={css["leftNav"]}>
