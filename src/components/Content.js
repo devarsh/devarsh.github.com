3ed749c6c78ad5a65926b6cc3b77da7304660ec3
devarsh shah <devarshmshah@gmail.com>
2017-11-14 20:24:19 +0530

    Added schooling

--- src/components/Content.js
+++ src/components/Content.js
@@ -24,7 +24,6 @@ export class Content extends Component {
           <li>
             <div className={css["header"]}>Education</div>
             <div className={css["col"]}>
-            { /*
               <div className={css["row"]}>
                 <div className={css["label"]}>
                   <b>1995-2007</b>
@@ -43,7 +42,6 @@ export class Content extends Component {
                   Somlalit Institue
                 </div>
               </div>
-            */}
               <div className={css["row"]}>
                 <div className={css["label"]}>
                   <b>2010-2013</b>
